00733ea993fe5b83561fa40686d40cfd1d981479
barbaraim <birarrazaval@buk.cl>
2023-01-11 12:30:20 -0300

    chore: add nickname to ignored columns on people

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,7 +1,7 @@
 class Person < ApplicationRecord
   has_many :tasks
 
-  self.ignored_columns = [:email] 
+  self.ignored_columns = [:email, :nickname] 
 
   def full_name
     "#{last_name}, #{first_name}"
